74b0f13fa8c1796a97db8cb63236c40a3edcde95
Simon <47429862+LautrecSec@users.noreply.github.com>
2023-06-07 11:39:46 -0600

    Update index.js
    Added nexus and steam app IDs

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// Nexus Mods domain for the game. e.g. nexusmods.com/darkestdungeon2
+const GAME_ID = 'darkestdungeon2';
+
+//Steam Application ID, you can get this from https://steamdb.info/apps/
+const STEAMAPP_ID = '1940340';
+
 //Import some assets from Vortex we'll need.
 const path = require('path');
 const { fs, log, util } = require('vortex-api');
